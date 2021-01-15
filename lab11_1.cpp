00349fad6c98305aa7f0f108ea3a213d18ddb2ac
minmeeen <sirapat.mp@gmail.com>
2021-01-15 15:30:10 +0700

    edit

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(int x);
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+
+    cout << fibonacci(50);
+
     return 0; 
+
+}
+
+long long int fibonacci(int x){
+    if(x>1){
+        return fibonacci(x-1) + fibonacci(x-2);
+    }else{
+        return x;
+    }
 }
