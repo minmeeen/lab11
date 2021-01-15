ee621b991a9b55236291939cd8e0dd294c4ea3df
minmeeen <sirapat.mp@gmail.com>
2021-01-15 16:08:29 +0700

    edit

--- lab11_4.cpp
+++ lab11_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename T>
+void mySwap(T &, T &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,14 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename T>
+void mySwap(T &a,T &b){
+	T i = b;
+	b = a;
+	a = i;
+	
+
 }
 
